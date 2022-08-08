7bad8c3d05af5b18afe99ef230f90c1b4fb133d8
beto1821 <beto1821@uol.com.br>
2022-08-08 19:34:58 -0300

    python refreshing bases

---
+++ def.py
@@ -0,0 +1,12 @@
+def hello(age, name):
+  return f'Hello {name} you are {age} years odl'
+
+print(hello(17, 'Mark'))
+
+
+def tripleprint(string):
+    print(string * 3)
+
+
+tripleprint("hello")
+
---
+++ list.py
@@ -0,0 +1,5 @@
+dognames = ["Fido", "Sean", "Sally", "Mark"]
+
+# del(dognames[2])
+
+print(len(dognames))
