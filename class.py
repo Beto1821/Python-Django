60d122060ad2f11bb08beae3f0a20cf6964b2eb6
beto1821 <beto1821@uol.com.br>
2022-08-08 20:59:21 -0300

    refreshing

---
+++ class.py
@@ -0,0 +1,22 @@
+class Dog:
+  
+    def __init__(self, name, age , furcolor):
+      self.name = name
+      self.age = age
+      self.furcolor = furcolor
+
+
+    def bark(self):
+      print("Dark!")
+
+mydog = Dog("Fido", 13, "Brown")
+
+print(mydog.age)
+
+
+
+# mydog.bark()
+# mydog.name = "Fido"
+# mydog.age = 16
+# print(mydog.name)
+# print(mydog.age)
